5ad0b48a3b69e291577b11bb4aeb641d0fd39b0a
ingalls <nick@ingalls.ca>
2026-02-27 15:09:16 -0700

    Add IconPath

--- index.js
+++ index.js
@@ -249,6 +249,10 @@ function createMainWindow(url) {
 }
 
 app.on('ready', () => {
+    if (app.dock) {
+        app.dock.setIcon(iconPath);
+    }
+
     const config = loadConfig();
     currentServerUrl = config.serverUrl || '';
     if (config.serverUrl) {
